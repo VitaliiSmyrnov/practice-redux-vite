465dbb3a14ffaf3d470d52df45ac4e5a2f0ab682
VitaliiSmyrnov <vitaliy.smirnov@ukr.net>
2023-07-22 14:11:20 +0300

    init: start practicing redux

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      assets: "/src/assets",
+      components: "/src/components",
+      pages: "/src/pages",
+      constants: "/src/constants",
+      db: "/src/db",
+      utils: "/src/utils",
+      hooks: "/src/hooks",
+      services: "/src/services",
+      redux: "/src/redux",
+    },
+  },
+});
